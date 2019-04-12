a94129feff531b59f2f195c5bce92269734590fd
Nolan Eastburn <nolieman1@gmail.com>
2019-04-12 12:41:37 -0500

    removed asset messages and modified some textures

--- PickleRickMod/ModEntry.cs
+++ PickleRickMod/ModEntry.cs
@@ -42,7 +42,7 @@ namespace PickleRickMod
         {
             if (asset.AssetNameEquals("Data/Furniture"))
             {
-                Monitor.Log($"Added asset in '{asset.AssetName}'.");
+                //Monitor.Log($"Added asset in '{asset.AssetName}'.");
                 IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
                 data[1306] = "Pickle Rick Statue - Color 1969/decor/1 2/1 1/1/500";
                 data[1305] = "Pickle Rick Statue/decor/1 2/1 1/1/500";
@@ -52,14 +52,14 @@ namespace PickleRickMod
 
             if (asset.AssetNameEquals("Data/Fish"))
             {
-                Monitor.Log($"Added asset in '{asset.AssetName}'.");
+                //Monitor.Log($"Added asset in '{asset.AssetName}'.");
                 IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
                 data[154] = "Pickle Rick/40/sinker/3/20/600 1900/fall winter/both/683 .2 689 .4/3/.25/.25/0";
             }
 
                 if (asset.AssetNameEquals("Data/ObjectInformation"))
                 {
-                    Monitor.Log($"Added asset in '{asset.AssetName}'.");
+                    //Monitor.Log($"Added asset in '{asset.AssetName}'.");
                     IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
                     data[154] = "Pickle Rick/75/30/Fish -4/Pickle Rick/Hey Morty look, I'm a pickle!/Day^Fall Winter";
                     data[113] = "Pickle Rick Statue/50/-300/Arch/Pickle Rick Statue/It's a statue of pickle rick! What demoic cult made this?/Farm .1/Decor 5 31";
@@ -67,19 +67,19 @@ namespace PickleRickMod
 
                 if (asset.AssetNameEquals("Maps/springobjects"))
                 {
-                    Monitor.Log($"Added asset in '{asset.AssetName}'.");
+                    //Monitor.Log($"Added asset in '{asset.AssetName}'.");
                     Texture2D image = this.Helper.Content.Load<Texture2D>("assets/picklerick.png", ContentSource.ModFolder);
                     asset.AsImage().PatchImage(image, targetArea: new Microsoft.Xna.Framework.Rectangle(160, 96, 16, 16));
 
                     image = this.Helper.Content.Load<Texture2D>("assets/artifactpicklerickstatue.png", ContentSource.ModFolder);
                     asset.AsImage().PatchImage(image, targetArea: new Microsoft.Xna.Framework.Rectangle(272, 64, 16, 16));
-                    Monitor.Log("IT WORKED!!!");
+                    //Monitor.Log("IT WORKED!!!");
 
                 }
 
                 if (asset.AssetNameEquals("TileSheets/furniture"))
                 {
-                    Monitor.Log($"Added asset in '{asset.AssetName}'.");
+                    //Monitor.Log($"Added asset in '{asset.AssetName}'.");
                     Texture2D image = this.Helper.Content.Load<Texture2D>("assets/picklerickstatuecolor.png", ContentSource.ModFolder);
                     asset.AsImage().PatchImage(image, targetArea: new Microsoft.Xna.Framework.Rectangle(416, 640, 16, 32));
 
